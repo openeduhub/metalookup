c65e46df59f498b8e1efbe2005d93f429affcdfc
Robert Meissner <robert.meissner@codecentric.de>
2020-11-03 08:06:24 +0100

    - Storing scraped testsuite websites in local folder

--- src/testsuite/suite.py
+++ src/testsuite/suite.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import requests
 from urllib.parse import urlparse
@@ -12,15 +13,23 @@ for url in urls:
     domain = urlparse(url).netloc
     filename = domain.replace(".", "_")
 
-    path = "/home/rcc/projects/WLO/oeh-search-etl/scraped/"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    path = "/".join(dir_path.split("/")[:-2])
+    for path_elements in ["/local_tools", "/testsuite", "/scraped/"]:
+        path += path_elements
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            pass
+
+    data = {}
+    data.update({"html": str(response.content)})
+    data.update({"headers": str(response.headers)})
+    data.update({"url": str(response.url)})
+
+    data.update({"json": str(response.json)})
+    data.update({"cookies": str(response.cookies)})
 
     with open(path + filename + ".log", "w") as file:
-        data = {}
-        data.update({"html": str(response.content)})
-        data.update({"headers": str(response.headers)})
-        data.update({"url": str(response.url)})
-
-        data.update({"json": str(response.json)})
-        data.update({"cookies": str(response.cookies)})
-
         file.write(json.dumps(data))
