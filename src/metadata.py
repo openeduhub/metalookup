d6896a8440c05fccb2bcf116716c467e5dc08565
RMeissnerCC <50482279+RMeissnerCC@users.noreply.github.com>
2020-11-03 10:09:55 +0100

    Easylist date update (#2)
    * - Reading "expires" and "last_modified" information from easylists
    
    * - Storing last modified and expiry dates in metadata dict
    - Converted regex strings to raw to resolve pylint issue
    
    * - Refactored calling __extract_date_from_list to be called by setup directly
    - Moved __prepare_tag_list in the if clause to check if url is not empty

--- src/metadata.py
+++ src/metadata.py
@@ -1,8 +1,12 @@
+import re
+
 import requests
 
 
 class Metadata:
     tag_list: list = []
+    tag_list_last_modified = ""
+    tag_list_expires: int = 0
     key: str = ""
     url: str = ""
     comment_symbol: str = ""
@@ -16,7 +20,8 @@ class Metadata:
             header = {}
         self._logger.info(f"Starting {self.__class__.__name__}")
         values = self._start(html_content=html_content, header=header)
-        return {self.key: values}
+        return {self.key: {"values": values, "tag_list_last_modified": self.tag_list_last_modified,
+                           "tag_list_expires": self.tag_list_expires}}
 
     def _start(self, html_content: str, header: dict) -> list:
         if self.evaluate_header:
@@ -38,6 +43,21 @@ class Metadata:
         else:
             self._logger.warning(f"Downloading tag list from '{self.url}' yielded status code '{result.status_code}'.")
 
+    def __extract_date_from_list(self):
+        expires_expression = re.compile(r"[!#:]\sExpires[:=]\s?(\d+)\s?\w{0,4}")
+        last_modified_expression = re.compile(r"[!#]\sLast modified:\s(\d\d\s\w{3}\s\d{4}\s\d\d:\d\d\s\w{3})")
+        for line in self.tag_list[0:10]:
+            match = last_modified_expression.match(line)
+            if match:
+                self.tag_list_last_modified = match.group(1)
+
+            match = expires_expression.match(line)
+            if match:
+                self.tag_list_expires = int(match.group(1))
+
+            if self.tag_list_last_modified != "" and self.tag_list_expires != 0:
+                break
+
     def __prepare_tag_list(self) -> None:
         self.tag_list = [i for i in self.tag_list if i != ""]
 
@@ -48,4 +68,5 @@ class Metadata:
         """Child function."""
         if self.url != "":
             self.__download_tag_list()
-        self.__prepare_tag_list()
+            self.__extract_date_from_list()
+            self.__prepare_tag_list()
