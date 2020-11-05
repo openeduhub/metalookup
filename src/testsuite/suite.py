4f0176e4643ee9d37813ae8619ac4f19ed19e327
Robert Meissner <robert.meissner@codecentric.de>
2020-11-05 09:50:33 +0100

    - Refactoring test suite

--- src/testsuite/suite.py
+++ src/testsuite/suite.py
@@ -4,32 +4,42 @@ from urllib.parse import urlparse
 
 import requests
 
-urls = ["https://www.dictionary.com/"]
+URLS = ["https://www.dictionary.com/"]
 
-# Making a get request
-for url in urls:
-    response = requests.get(url)
 
-    domain = urlparse(url).netloc
-    filename = domain.replace(".", "_")
+def main():
+    """
+    Download specific websites for testing purpose and store them locally
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    :return:
+    """
+    for url in URLS:
+        response = requests.get(url)
 
-    path = "/".join(dir_path.split("/")[:-2])
-    for path_elements in ["/local_tools", "/testsuite", "/scraped/"]:
-        path += path_elements
-        try:
-            os.mkdir(path)
-        except FileExistsError:
-            pass
+        domain = urlparse(url).netloc
+        filename = domain.replace(".", "_")
 
-    data = {}
-    data.update({"html": str(response.content)})
-    data.update({"headers": str(response.headers)})
-    data.update({"url": str(response.url)})
+        dir_path = os.path.dirname(os.path.realpath(__file__))
 
-    data.update({"json": str(response.json)})
-    data.update({"cookies": str(response.cookies)})
+        path = "/".join(dir_path.split("/")[:-2])
+        for path_elements in ["/local_tools", "/testsuite", "/scraped/"]:
+            path += path_elements
+            try:
+                os.mkdir(path)
+            except FileExistsError:
+                pass
 
-    with open(path + filename + ".log", "w") as file:
-        file.write(json.dumps(data))
+        data = {}
+        data.update({"html": str(response.content)})
+        data.update({"headers": str(response.headers)})
+        data.update({"url": str(response.url)})
+
+        data.update({"json": str(response.json)})
+        data.update({"cookies": str(response.cookies)})
+
+        with open(path + filename + ".log", "w") as file:
+            file.write(json.dumps(data))
+
+
+if __name__ == "__main__":
+    main()
