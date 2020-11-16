2fcc39910d658af572e64ede8a3d2ede42b42f28
Robert Meissner <robert.meissner@codecentric.de>
2020-11-16 13:34:06 +0100

    - Added probability calculation for accessibility - Added API key (no secret)

--- src/features/accessibility.py
+++ src/features/accessibility.py
@@ -3,16 +3,24 @@ from json import JSONDecodeError
 
 import requests
 
-from features.metadata_base import MetadataBase
+from features.metadata_base import MetadataBase, ProbabilityDeterminationMethod
 from features.website_manager import WebsiteData
 from lib.constants import VALUES
 
 
 class Accessibility(MetadataBase):
+    probability_determination_method = (
+        ProbabilityDeterminationMethod.FIRST_VALUE
+    )
+    decision_threshold = 0.8
+
     def _start(self, website_data: WebsiteData) -> dict:
         process = requests.get(
             "https://www.googleapis.com/pagespeedonline/v5/runPagespeed",
-            params={"url": f"{website_data.url}", "category": "ACCESSIBILITY"},
+            params={
+                "url": f"{website_data.url}",
+                "category": "ACCESSIBILITY",
+            },
         )
         try:
             result = json.loads(process.content.decode())
