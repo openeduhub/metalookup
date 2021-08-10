46ae9325e9e350ae6e8f2723c5cf1750f289f97b
RMeissner <robert.meissner@codecentric.de>
2021-08-10 10:21:03 +0200

    test_production: Adding unit test for db

--- tests/unit/db/db_test.py
+++ tests/unit/db/db_test.py
@@ -5,6 +5,7 @@ import sqlalchemy.exc
 
 from app.schemas import RecordSchema
 from db.db import create_dummy_record, load_cache
+from db.models import CacheEntry
 
 
 @pytest.fixture
@@ -50,9 +51,21 @@ def test_load_cache():
     assert empty_cache_loaded
 
     with mock.patch("db.db.SessionLocal") as session_local:
-        session_local().__enter__().query().all.side_effect = sqlalchemy.exc.SQLAlchemyError
+        session_local().__enter__().query().all.side_effect = sqlalchemy.exc.SQLAlchemyError("Unit test")
         output = load_cache()
         empty_cache_loaded = output == empty_cache
 
     assert empty_cache_loaded
+
+    entry = CacheEntry()
+
+    entry.top_level_domain = "test_domain"
+    example_cache_entry = [entry]
+    with mock.patch("db.db.SessionLocal") as session_local:
+        session_local().__enter__().query().all.return_value = example_cache_entry
+        output = load_cache()
+        cache_loaded = output == example_cache_entry
+        correct_domain = output[0].top_level_domain == entry.top_level_domain
+
+    assert cache_loaded and correct_domain
 
