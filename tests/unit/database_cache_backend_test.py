3dd3330dba88dbeca7834c5a892e5f8135662a03
Martin Rueckl <martin.rueckl@codecentric.de>
2022-05-31 14:04:17 +0200

    replace foobar values with reasonable naming

--- tests/unit/database_cache_backend_test.py
+++ tests/unit/database_cache_backend_test.py
@@ -10,26 +10,36 @@ async def test_backend(tmpdir):
     backend = DatabaseBackend(url=f"sqlite:///{tmpdir}/test.db")
     await backend.setup()
 
-    assert await backend.get("foo") is None
-    await backend.set("foo", "bar", 100)
-    assert await backend.get("foo") == "bar"
-    assert await backend.get_with_ttl("foo") == (100, "bar")
-    time.sleep(3)
-    assert await backend.get_with_ttl("foo") == (97, "bar")
+    assert await backend.get(key="some-key") is None
+    await backend.set(key="some-key", value="some-value", expire=100)
+    assert await backend.get(key="some-key") == "some-value"
+    expire, value = await backend.get_with_ttl(key="some-key")
+    assert expire <= 100
+    assert value == "some-value"
+    time.sleep(1)
+    expire, value = await backend.get_with_ttl(key="some-key")
+    assert expire <= 99
+    assert value == "some-value"
 
 
 @pytest.mark.asyncio
 async def test_backend_postgres():
     # uses the configuration from docker-compose.yaml db container
-    backend = DatabaseBackend(url="postgresql://postgres:postgres@localhost/storage")
+    backend = DatabaseBackend(url="postgresql://metalookup:metalookup@localhost/metalookup")
     try:
         await backend.setup()
     except OSError as e:
         print(e)
         pytest.skip("Failed to connect to postgres, probably container is not running. Skipping test")
 
-    await backend.set("foo", "bar", 100)
-    assert await backend.get("foo") == "bar"
-    assert await backend.get_with_ttl("foo") == (100, "bar")
-    time.sleep(3)
-    assert await backend.get_with_ttl("foo") == (97, "bar")
+    await backend.set(key="some-key", value="some-value", expire=100)
+    assert await backend.get(key="some-key") == "some-value"
+    expire, value = await backend.get_with_ttl(key="some-key")
+    assert expire <= 100
+    assert value == "some-value"
+
+    time.sleep(1)
+
+    expire, value = await backend.get_with_ttl(key="some-key")
+    assert expire <= 99
+    assert value == "some-value"
