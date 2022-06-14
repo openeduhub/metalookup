f2307ca3f51491a70868f18c45068ba08a85a3ec
Martin Rueckl <martin.rueckl@codecentric.de>
2022-06-14 14:41:31 +0200

    simplify and transition ExtractFromFiles to Extractor base class
    - resolves #101 by keeping the file in memory instead of persisting on disk
    - decouples from MetadataBase
    - improve tests by mocking only the download part
    - potentially fixed bug where the extractor always returned 5 stars
    - updated to new api of pdf packages (resolved deprecation warnings)
    - dispatch potentially cpu intensive scanning to process pool to prevent
      blocking the service async event loop

--- src/features/extract_from_files.py
+++ src/features/extract_from_files.py
@@ -1,27 +1,38 @@
 import asyncio
 import base64
+import io
+import logging
 import os
 import zipfile
+from concurrent.futures import Executor
+from typing import BinaryIO, Iterable, Optional
 from urllib.parse import urlparse
 
 import PyPDF2
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from pdfminer.high_level import extract_text
-from PyPDF2.utils import PdfReadError
+from PyPDF2.errors import PdfReadError
 
 from app.models import Explanation, StarCase
-from core.extractor import MetadataBase
+from core.extractor import Extractor
 from core.website_manager import WebsiteData
 from lib.settings import RETURN_IMAGES_IN_METADATA
 
-_FOUND_EXTRACTABLE_FILES = "Found extractable files"
-_FOUND_NO_EXTRACTABLE_FILES = "Found no extractable files"
+logger = logging.getLogger(__file__)
 
 
-@MetadataBase.with_key()
-class ExtractFromFiles(MetadataBase):
-    decision_threshold = 0.5
+_CONTENT_SIZE_THRESHOLD = 25  # if a downloaded content is larger than X megabytes it will not be analyzed
+_NO_FILES_TO_EXTRACT = "No files to extract"
+_SUFFICIENT_AMOUNT_OF_FILES_EXTRACTABLE = "Could extract content from most extractable files"
+_INSUFFICIENT_AMOUNT_OF_FILES_EXTRACTABLE = "Could not extract content from the required fraction of extractable files"
+
+
+class ExtractFromFiles(Extractor[set[str]]):
+    key = "extract_from_files"
+
+    threshold = 0.5
+    """The fraction of files that need to be readable to yield a 5 star rating."""
 
     xmp_metadata = [
         "dc_contributor",
@@ -50,6 +61,26 @@ class ExtractFromFiles(MetadataBase):
         "xmpmm_instanceId",
     ]
 
+    async def setup(self):
+        pass
+
+    async def extract(self, site: WebsiteData, executor: Executor) -> tuple[StarCase, Explanation, set[str]]:
+        extractable_files = self._get_extractable_files(site)
+
+        if len(extractable_files) == 0:  # avoid division by zero error below
+            return StarCase.FIVE, _NO_FILES_TO_EXTRACT, set()
+
+        values = await self._work_files(files=extractable_files, executor=executor)
+
+        fraction = len(values) / len(extractable_files)
+        explanation = (
+            _SUFFICIENT_AMOUNT_OF_FILES_EXTRACTABLE
+            if fraction >= self.threshold
+            else _INSUFFICIENT_AMOUNT_OF_FILES_EXTRACTABLE
+        )
+        stars = StarCase.FIVE if fraction >= self.threshold else StarCase.ZERO
+        return stars, explanation, values
+
     @staticmethod
     def _get_xml_body(soup: BeautifulSoup, xml_file: zipfile.ZipInfo) -> BeautifulSoup:
         body = BeautifulSoup()
@@ -75,8 +106,8 @@ class ExtractFromFiles(MetadataBase):
         image = base64.b64encode(image).decode()
         return {xml_file.filename: image}
 
-    def _extract_docx(self, filename: str) -> dict:
-        document = zipfile.ZipFile(filename)
+    def _extract_docx(self, file: BinaryIO) -> tuple[list[str], dict[str, str]]:
+        document = zipfile.ZipFile(file=file)
 
         extracted_content = []
         images = {}
@@ -87,99 +118,93 @@ class ExtractFromFiles(MetadataBase):
             elif RETURN_IMAGES_IN_METADATA and file.filename.find("media") >= 0:
                 images.update(self._extract_image_content(document, file))
 
-        return {"extracted_content": extracted_content, "images": images}
+        return extracted_content, images
 
-    def _get_pdf_content(self, filename: str, pdf_file: PyPDF2.PdfFileReader) -> str:
-        extracted_content = f"{pdf_file.getDocumentInfo()}"
-        data = pdf_file.getXmpMetadata()
+    def _get_pdf_content(self, file: BinaryIO, pdf_file: PyPDF2.PdfFileReader) -> str:
+        extracted_content = f"{pdf_file.metadata}"
+        data = pdf_file.xmp_metadata
         for parameter in self.xmp_metadata:
             if parameter:
                 try:
                     extracted_content += f"{parameter}, {getattr(data, parameter)}"
                 except (AttributeError, TypeError) as err:
-                    self._logger.exception(
-                        f"Parameter in pdf content failed to be retrieved: {parameter} with {err.args}"
-                    )
-        extracted_content += extract_text(filename)
+                    logger.exception(f"Parameter in pdf content failed to be retrieved: {parameter} with {err.args}")
+        extracted_content += extract_text(file)
         return extracted_content
 
     @staticmethod
-    def _get_pdf_images(pdf_file: PyPDF2.PdfFileReader) -> list:
-        images = []
+    def _get_pdf_images(reader: PyPDF2.PdfFileReader) -> list:
         resources_tag = "/Resources"
         xobject_tag = "/XObject"
         subtype_tag = "/Subtype"
-        for page in range(pdf_file.getNumPages()):
-            pdf_page = pdf_file.getPage(page)
-            if resources_tag in pdf_page.keys() and xobject_tag in pdf_page[resources_tag].keys():
-                x_object = pdf_page[resources_tag][xobject_tag].getObject()
 
-                for obj in x_object:
-                    if x_object[obj][subtype_tag] == "/Image":
-                        images += obj
-        return images
+        def images(x_object) -> Iterable[str]:
+            for obj in x_object:
+                if x_object[obj][subtype_tag] == "/Image":
+                    yield obj
 
-    def _extract_pdfs(self, filename: str) -> dict:
+        def x_objects():
+            for page in reader.pages:
+                if resources_tag in page.keys() and xobject_tag in page[resources_tag].keys():
+                    yield page[resources_tag][xobject_tag].get_object()
+
+        return [image for x_object in x_objects() for image in images(x_object)]
+
+    def _extract_pdfs(self, file: BinaryIO) -> tuple[str, list[str]]:
         try:
-            pdf_file = PyPDF2.PdfFileReader(open(filename, "rb"))
-            extracted_content = self._get_pdf_content(filename, pdf_file)
+            pdf_file = PyPDF2.PdfFileReader(file)
+            extracted_content = self._get_pdf_content(file, pdf_file)
             images = self._get_pdf_images(pdf_file)
+            return extracted_content, images
         except (PdfReadError, OSError):
-            extracted_content = []
-            images = []
+            return "", []
 
-        return {"extracted_content": extracted_content, "images": images}
-
-    async def _download_file(self, file_url: str, filename: str, session: ClientSession) -> None:
-        result = await session.get(url=file_url)
+    async def _download_file(self, file: str, session: ClientSession) -> BinaryIO:
+        result = await session.get(url=file)
         if result.status != 200:
-            self._logger.exception(f"Downloading file '{file_url}' yielded status code '{result.status}'.")
-        open(filename, "wb").write(await result.read())
+            logger.exception(f"Downloading file '{file}' yielded status code '{result.status}'.")
+        if result.content_length is not None and result.content_length > _CONTENT_SIZE_THRESHOLD * 1024 * 1024:
+            logger.info(f"Skipping processing of full content for {file=} with {result.content_length=}")
+            return io.BytesIO(b"<skipped>")
+        return io.BytesIO(await result.read())
 
-    async def _process_file(self, file: str, session: ClientSession) -> str:
-        filename = os.path.basename(urlparse(file).path)
-        extension = filename.split(".")[-1]
-        await self._download_file(file, filename, session)
-
-        content = {"extracted_content": [], "images": {}}
+    def _process_file(self, file: BinaryIO, extension: str) -> bool:
+        """
+        Returns true whether some content could be extracted from the file.
+        This function may be relatively CPU heavy and can hence be dispatched to an executor pool.
+        """
         if extension == "docx":
-            content = self._extract_docx(filename)
+            content, _ = self._extract_docx(file)
         elif extension == "pdf":
-            content = self._extract_pdfs(filename)
+            content, _ = self._extract_pdfs(file)
+        else:
+            raise ValueError(f"Unsupported extension {extension}")
 
-        os.remove(filename)
+        return len(content) > 0
 
-        if len(content["extracted_content"]) == 0:
-            filename = ""
-        return filename
-
-    async def _work_files(self, files: list) -> list[str]:
+    async def _work_files(self, executor: Executor, files: list) -> set[str]:
         async with ClientSession() as session:
-            tasks = [self._process_file(file, session) for file in files]
-            extractable_files = await asyncio.gather(*tasks)
 
-        return [file for file in extractable_files if file != ""]
+            async def task(url: str) -> Optional[str]:
+                extension = os.path.basename(urlparse(url).path).split(".")[-1]
+                if extension not in {"docx", "pdf"}:
+                    return None
+
+                file = await self._download_file(url, session)
+
+                loop = asyncio.get_running_loop()
+                success = await loop.run_in_executor(executor, self._process_file, file, extension)
+                return url if success else None
+
+            tasks = [task(url=file) for file in files]
+            extractable_files: tuple[Optional[str], ...] = await asyncio.gather(*tasks)
+
+        return {file for file in extractable_files if file is not None}
 
     @staticmethod
-    def _get_extractable_files(website_data: WebsiteData) -> list:
+    def _get_extractable_files(website_data: WebsiteData) -> set[str]:
         file_extensions = [os.path.splitext(link)[-1] for link in website_data.raw_links]
 
-        extractable_files = [
+        return {
             file for file, extension in zip(website_data.raw_links, file_extensions) if extension in [".docx", ".pdf"]
-        ]
-
-        return extractable_files
-
-    async def _start(self, website_data: WebsiteData) -> list[str]:
-        extractable_files = self._get_extractable_files(website_data)
-        return await self._work_files(files=extractable_files)
-
-    def _decide(self, website_data: WebsiteData) -> tuple[StarCase, Explanation]:
-        probability = 0
-        extractable_files = self._get_extractable_files(website_data)
-
-        if website_data.values:
-            probability = len(extractable_files) / len(website_data.values)
-        decision = self._get_inverted_decision(probability)
-        explanation = _FOUND_EXTRACTABLE_FILES if decision == StarCase.FIVE else _FOUND_NO_EXTRACTABLE_FILES
-        return decision, explanation
+        }
